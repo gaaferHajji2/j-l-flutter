57e007b360aa518f68fbeaa3921cfdaa32e7e951
Jafar Loka <gaafer.hajji1995@gmail.com>
2025-07-27 21:00:33 +0300

    change price && adding getx controller for products

---
+++ flutter_application_8/lib/app/modules/product/controllers/product_controller.dart
@@ -0,0 +1,18 @@
+import 'package:flutter_application_8/app/modules/product/models/product.dart';
+import 'package:get/get.dart';
+
+class ProductController extends GetxController {
+  var products = <ProductModel>[].obs;
+
+  @override
+  void onInit() {
+    products.addAll([
+      ProductModel(name: "JLoka Product-01", price: 150.5),
+      ProductModel(name: "JLoka Product-01", price: 200.5),
+      ProductModel(name: "JLoka Product-01", price: 300.5),
+    ])
+
+    super.onInit();
+  }
+
+}
--- flutter_application_8/lib/app/modules/product/models/product.dart
+++ flutter_application_8/lib/app/modules/product/models/product.dart
@@ -1,6 +1,6 @@
 class ProductModel {
   final String name;
-  final String price;
+  final double price;
 
   ProductModel({required this.name, required this.price});
 }
