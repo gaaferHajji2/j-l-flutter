7e6344ab489629c65a766ad8496e4d14afb90b9f
Jafar Loka <gaafer.hajji1995@gmail.com>
2025-07-27 22:31:20 +0300

    adding checkout view

---
+++ flutter_application_8/lib/app/modules/product/views/checkout_view.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:get/get.dart';
+
+class CheckoutView extends StatelessWidget {
+  const CheckoutView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Checkout Page"), centerTitle: true),
+
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text("Thanks For purchase"),
+
+            SizedBox(height: 16.0),
+
+            ElevatedButton(
+              onPressed: () {
+                Get.offAllNamed("/product");
+              },
+              child: Text("Go to product"),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
