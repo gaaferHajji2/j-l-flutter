db78360684acbd71f962d456865e15d6058c5421
Jafar Loka <gaafer.hajji1995@gmail.com>
2025-08-01 14:57:45 +0300

    Adding product service to product controller

--- flutter_application_8/lib/app/binds/product_bindings.dart
+++ flutter_application_8/lib/app/binds/product_bindings.dart
@@ -1,11 +1,12 @@
 import 'package:flutter_application_8/app/modules/product/controllers/cart_controller.dart';
 import 'package:flutter_application_8/app/modules/product/controllers/product_controller.dart';
+import 'package:flutter_application_8/app/services/product_service.dart';
 import 'package:get/get.dart';
 
 class ProductBindings extends Bindings {
   @override
   void dependencies() {
-    Get.put(ProductController());
+    Get.put(ProductController(productService: ProductService()));
     Get.put(CartController());
   }
 }
--- flutter_application_8/lib/app/modules/product/controllers/product_controller.dart
+++ flutter_application_8/lib/app/modules/product/controllers/product_controller.dart
@@ -1,8 +1,13 @@
 import 'package:flutter_application_8/app/modules/product/models/product.dart';
+import 'package:flutter_application_8/app/services/product_service.dart';
 import 'package:get/get.dart';
 
 class ProductController extends GetxController {
   var products = <ProductModel>[].obs;
+
+  final ProductService productService;
+
+  ProductController({required this.productService});
 
   @override
   void onInit() {
