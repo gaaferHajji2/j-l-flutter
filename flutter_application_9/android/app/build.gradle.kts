d8ef5510fbf8de642b838fa478e802643ee95630
Jafar Loka <gaafer.hajji1995@gmail.com>
2025-08-03 22:14:11 +0300

    adding firebase dep. and init GetStorage

--- flutter_application_9/android/app/build.gradle.kts
+++ flutter_application_9/android/app/build.gradle.kts
@@ -3,12 +3,26 @@ plugins {
     id("kotlin-android")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
+
+    // Add the Google services Gradle plugin
+    id("com.google.gms.google-services")
+
+}
+
+dependencies {
+  // Import the Firebase BoM
+  implementation(platform("com.google.firebase:firebase-bom:34.0.0"))
+
+
+  // TODO: Add the dependencies for Firebase products you want to use
+  // When using the BoM, don't specify versions in Firebase dependencies
+  // https://firebase.google.com/docs/android/setup#available-libraries
 }
 
 android {
     namespace = "com.example.flutter_application_9"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
