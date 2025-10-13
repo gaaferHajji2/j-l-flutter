a0e51f3c908406ed57e47820294a3956d1311fab
Jafar Loka <gaafer.hajji1995@gmail.com>
2025-10-13 04:57:30 +0300

    Remove unused projects

--- flutter_application_9/android/app/build.gradle.kts
+++ flutter_application_9/android/app/build.gradle.kts
@@ -3,26 +3,12 @@ plugins {
     id("kotlin-android")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
-
-    // Add the Google services Gradle plugin
-    id("com.google.gms.google-services")
-
-}
-
-dependencies {
-  // Import the Firebase BoM
-  implementation(platform("com.google.firebase:firebase-bom:34.0.0"))
-
-
-  // TODO: Add the dependencies for Firebase products you want to use
-  // When using the BoM, don't specify versions in Firebase dependencies
-  // https://firebase.google.com/docs/android/setup#available-libraries
 }
 
 android {
     namespace = "com.example.flutter_application_9"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = "27.0.12077973"
+    ndkVersion = flutter.ndkVersion
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
@@ -35,7 +21,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.jloka.delivero"
+        applicationId = "com.example.flutter_application_9"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
