23b02c3e74957abd0f6b1b9dd0ba87437967c202
JafarLoka <gaafer.hajji1995@gmail.com>
2026-04-11 18:49:32 +0300

    Build product entity

---
+++ flutter_shop/lib/features/product/domain/entities/product.dart
@@ -0,0 +1,23 @@
+// MODEL + EQUATABLE for BLoC comparison
+import 'package:equatable/equatable.dart';
+
+class Product extends Equatable {
+  final String id;
+  final String name;
+  final String description;
+  final double price;
+  final String imageUrl;
+  final String category;
+
+  const Product({
+    required this.id,
+    required this.name,
+    required this.description,
+    required this.price,
+    required this.imageUrl,
+    required this.category,
+  });
+
+  @override
+  List<Object?> get props => [id, name, price, imageUrl, category];
+}
