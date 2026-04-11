ecee8ea75c2de51769671dfb8923de5693ad0e1f
JafarLoka <gaafer.hajji1995@gmail.com>
2026-04-11 19:17:45 +0300

    Build home page & change injection

--- flutter_shop/lib/core/di/injection.dart
+++ flutter_shop/lib/core/di/injection.dart
@@ -1,5 +1,8 @@
+import 'package:flutter_shop/features/product/data/repositories/product_repository_impl.dart';
+
 class Injection {
-  static final ProductRepository productRepository = ProductRepositoryImpl();
-  static final CartRepository cartRepository = CartRepositoryImpl();
-  static final OrderRepository orderRepository = OrderRepositoryImpl();
+  static final ProductRepositoryImpl productRepository =
+      ProductRepositoryImpl();
+  // static final CartRepository cartRepository = CartRepositoryImpl();
+  // static final OrderRepository orderRepository = OrderRepositoryImpl();
 }
