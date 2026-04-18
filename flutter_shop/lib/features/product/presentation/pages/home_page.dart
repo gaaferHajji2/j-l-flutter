5a339ee25a9670415f57014998376cc2b811d805
JafarLoka <gaafer.hajji1995@gmail.com>
2026-04-18 14:47:27 +0300

    Set title

--- flutter_shop/lib/features/product/presentation/pages/home_page.dart
+++ flutter_shop/lib/features/product/presentation/pages/home_page.dart
@@ -13,7 +13,7 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('FlutterShop')),
+      appBar: AppBar(title: const Text('Flutter Shop')),
       body: BlocProvider(
         create: (_) =>
             ProductBloc(Injection.productRepository)..add(LoadProducts()),
