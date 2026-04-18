79a7501fd2f1d34985a5ccd67866c6c4e9cd3d99
JafarLoka <gaafer.hajji1995@gmail.com>
2026-04-18 14:37:02 +0300

    Adding app theme to main application

--- flutter_shop/lib/main.dart
+++ flutter_shop/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_shop/core/theme/app_theme.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,7 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(colorScheme: .fromSeed(seedColor: Colors.deepPurple)),
+      theme: appTheme,
       home: const Scaffold(),
     );
   }
