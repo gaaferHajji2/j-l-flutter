fabeabb160e8b9287cae840fe16c3d16bfd05664
JafarLoka <gaafer.hajji1995@gmail.com>
2026-04-18 15:17:48 +0300

    Adding cart page with cart injection

--- flutter_shop/lib/core/di/injection.dart
+++ flutter_shop/lib/core/di/injection.dart
@@ -1,8 +1,10 @@
+import 'package:flutter_shop/features/cart/data/repositories/cart_repositories_impl.dart';
+import 'package:flutter_shop/features/cart/domain/repositories/cart_repositories.dart';
 import 'package:flutter_shop/features/product/data/repositories/product_repository_impl.dart';
 
 class Injection {
   static final ProductRepositoryImpl productRepository =
       ProductRepositoryImpl();
-  // static final CartRepository cartRepository = CartRepositoryImpl();
+  static final CartRepository cartRepository = CartRepositoryImpl();
   // static final OrderRepository orderRepository = OrderRepositoryImpl();
 }
