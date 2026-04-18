d1cf944677b4e895ca255b95cee09f9ea8fe7ed4
JafarLoka <gaafer.hajji1995@gmail.com>
2026-04-18 15:02:55 +0300

    Build cart bloc

---
+++ flutter_shop/lib/features/cart/presentation/bloc/cart_bloc.dart
@@ -0,0 +1,45 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_shop/features/cart/domain/repositories/cart_repositories.dart';
+
+class CartBloc extends Bloc<CartEvent, CartState> {
+  final CartRepository repository;
+
+  CartBloc(this.repository) : super(CartInitial()) {
+    on<AddToCart>(_onAddToCart);
+    on<RemoveFromCart>(_onRemoveFromCart);
+    on<UpdateQuantity>(_onUpdateQuantity);
+    on<ClearCart>(_onClearCart);
+    // Initial load
+    _loadCart();
+  }
+
+  void _loadCart() {
+    emit(CartLoaded(repository.getCartItems(), repository.getTotal()));
+  }
+
+  Future<void> _onAddToCart(AddToCart event, Emitter<CartState> emit) async {
+    repository.addItem(event.item);
+    emit(CartLoaded(repository.getCartItems(), repository.getTotal()));
+  }
+
+  Future<void> _onRemoveFromCart(
+    RemoveFromCart event,
+    Emitter<CartState> emit,
+  ) async {
+    repository.removeItem(event.productId);
+    emit(CartLoaded(repository.getCartItems(), repository.getTotal()));
+  }
+
+  Future<void> _onUpdateQuantity(
+    UpdateQuantity event,
+    Emitter<CartState> emit,
+  ) async {
+    repository.updateQuantity(event.productId, event.quantity);
+    emit(CartLoaded(repository.getCartItems(), repository.getTotal()));
+  }
+
+  Future<void> _onClearCart(ClearCart event, Emitter<CartState> emit) async {
+    repository.clearCart();
+    emit(CartLoaded([], 0));
+  }
+}
