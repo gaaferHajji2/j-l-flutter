e68d7fdd4517388da7e11eb3d1afc83f5a805b88
JafarLoka <gaafer.hajji1995@gmail.com>
2026-04-19 11:23:11 +0300

    Update main flutter shop file

--- flutter_shop/lib/main.dart
+++ flutter_shop/lib/main.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_shop/core/di/injection.dart';
 import 'package:flutter_shop/core/theme/app_theme.dart';
+import 'package:flutter_shop/features/cart/presentation/bloc/cart_bloc.dart';
+import 'package:flutter_shop/features/order/presentation/bloc/order_bloc.dart';
+import 'package:flutter_shop/router.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,10 +16,17 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: appTheme,
-      home: const Scaffold(),
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(create: (_) => CartBloc(Injection.cartRepository)),
+        BlocProvider(create: (_) => OrderBloc(Injection.orderRepository)),
+      ],
+      child: MaterialApp.router(
+        title: 'FlutterShop',
+        theme: appTheme,
+        routerConfig: router,
+        debugShowCheckedModeBanner: false,
+      ),
     );
   }
 }
