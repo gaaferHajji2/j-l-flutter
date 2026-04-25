3e0e6547d2dd9bf1062c6bbf099d92f4f07f8935
JafarLoka <gaafer.hajji1995@gmail.com>
2026-04-25 13:13:21 +0300

    Remove the debug banner

--- flutter_bloc_example/lib/main.dart
+++ flutter_bloc_example/lib/main.dart
@@ -12,6 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(colorScheme: .fromSeed(seedColor: Colors.deepPurple)),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
