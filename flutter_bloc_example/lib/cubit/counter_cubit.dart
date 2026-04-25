2e4f8e91c54d3cc023392db5645ef9ae8f184369
JafarLoka <gaafer.hajji1995@gmail.com>
2026-04-25 12:58:19 +0300

    Build the increment state function

--- flutter_bloc_example/lib/cubit/counter_cubit.dart
+++ flutter_bloc_example/lib/cubit/counter_cubit.dart
@@ -4,4 +4,8 @@ class CounterCubit extends Cubit<int> {
   // here we must create the default constructor and initialize it with the initial state
   // we initialize the constructor with initial state because cubit has it
   CounterCubit() : super(0);
+
+  void increment() {
+    emit(state + 1);
+  }
 }
