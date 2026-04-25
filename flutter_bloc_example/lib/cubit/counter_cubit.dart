75e1487e1b25749da51cc5d6b2dec0596e7e4a42
JafarLoka <gaafer.hajji1995@gmail.com>
2026-04-25 12:51:23 +0300

    Create the counter cubit class

---
+++ flutter_bloc_example/lib/cubit/counter_cubit.dart
@@ -0,0 +1,7 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class CounterCubit extends Cubit {
+  // here we must create the default constructor and initialize it with the initial state
+  // we initialize the constructor with initial state because cubit has it
+  CounterCubit() : super(0);
+}
