352fa4e2b0bca730f7ef6f75e001a3ff0d7f034b
JafarLoka <gaafer.hajji1995@gmail.com>
2026-04-26 11:44:10 +0300

    Adding more event with test

--- flutter_bloc_example/lib/bloc/counter_bloc.dart
+++ flutter_bloc_example/lib/bloc/counter_bloc.dart
@@ -9,11 +9,16 @@ class CounterDecrement extends CounterEvent {}
 class CounterBloc extends Bloc<CounterEvent, int> {
   CounterBloc() : super(0) {
     on<CounterIncrement>((event, emit) => increment(event, emit));
+    on<CounterDecrement>((event, emit) => decrement(event, emit));
 
     // on<CounterIncrement>(increment);
   }
 
   void increment(CounterIncrement event, Emitter<int> emit) {
     emit(state + 1);
+  }
+
+  void decrement(CounterDecrement event, Emitter<int> emit) {
+    emit(state - 1);
   }
 }
--- flutter_bloc_example/lib/home_page.dart
+++ flutter_bloc_example/lib/home_page.dart
@@ -34,10 +34,23 @@ class MyHomePage extends StatelessWidget {
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () => context.read<CounterBloc>().add(CounterIncrement()),
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+      floatingActionButton: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          FloatingActionButton(
+            onPressed: () =>
+                context.read<CounterBloc>().add(CounterIncrement()),
+            tooltip: 'Increment',
+            child: const Icon(Icons.add),
+          ),
+
+          FloatingActionButton(
+            onPressed: () =>
+                context.read<CounterBloc>().add(CounterDecrement()),
+            tooltip: 'Decrement',
+            child: const Icon(Icons.remove),
+          ),
+        ],
       ),
     );
   }
