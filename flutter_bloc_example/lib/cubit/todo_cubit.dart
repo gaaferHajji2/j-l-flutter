85aeac1ffa9bdb7bf2b2c8a636d0b8b706577c61
JafarLoka <gaafer.hajji1995@gmail.com>
2026-05-02 13:22:01 +0300

    Create add todo item

--- flutter_bloc_example/lib/cubit/todo_cubit.dart
+++ flutter_bloc_example/lib/cubit/todo_cubit.dart
@@ -3,4 +3,10 @@ import 'package:flutter_bloc_example/models/todo_model.dart';
 
 class TodoCubit extends Cubit<List<Todo>> {
   TodoCubit() : super([]);
+
+  void addTodo(String name) {
+    final todo = Todo(name: name, createdAt: DateTime.now());
+    state.add(todo);
+    emit(state);
+  }
 }
