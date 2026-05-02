9e3ca9823fd743c84133f0e6a686ad578d7d5b01
JafarLoka <gaafer.hajji1995@gmail.com>
2026-05-02 13:08:44 +0300

    Init the todo list

--- flutter_bloc_example/lib/todo_list.dart
+++ flutter_bloc_example/lib/todo_list.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_bloc_example/cubit/todo_cubit.dart';
 
 class TodoList extends StatelessWidget {
   const TodoList({super.key});
@@ -6,10 +8,11 @@ class TodoList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Todo List'),
+      appBar: AppBar(title: const Text('Todo List')),
+      body: ListView.builder(
+        itemCount: context.read<TodoCubit>().state.length,
+        itemBuilder: (BuildContext context, int index) {},
       ),
-      // body: ,
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           Navigator.pushNamed(context, '/add-todo');
