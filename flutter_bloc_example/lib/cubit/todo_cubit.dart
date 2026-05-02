70bb03de55c08eebd185bc84ac2e88b30250fe2f
JafarLoka <gaafer.hajji1995@gmail.com>
2026-05-02 13:42:08 +0300

    Add print statement

--- flutter_bloc_example/lib/cubit/todo_cubit.dart
+++ flutter_bloc_example/lib/cubit/todo_cubit.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_bloc_example/models/todo_model.dart';
 
@@ -8,5 +9,11 @@ class TodoCubit extends Cubit<List<Todo>> {
     final todo = Todo(name: name, createdAt: DateTime.now());
     // [emit] does nothing if the [state] being emitted is equal to the current [state].
     emit([...state, todo]);
+  }
+
+  @override
+  void onChange(Change<List<Todo>> change) {
+    super.onChange(change);
+    debugPrint(change.toString());
   }
 }
