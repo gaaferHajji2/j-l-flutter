5f13b686ee255b33701e6f1bff4d20d07eb450c6
JafarLoka <gaafer.hajji1995@gmail.com>
2026-05-02 13:03:18 +0300

    Create the todo cubit

---
+++ flutter_bloc_example/lib/cubit/todo_cubit_cubit.dart
@@ -0,0 +1,6 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_bloc_example/models/todo_model.dart';
+
+class TodoCubitCubit extends Cubit<List<Todo>> {
+  TodoCubitCubit() : super([]);
+}
