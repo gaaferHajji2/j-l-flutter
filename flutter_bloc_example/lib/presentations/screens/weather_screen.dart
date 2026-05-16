c128755b7cdecd33f5a627260c29c0168539887b
JafarLoka <gaafer.hajji1995@gmail.com>
2026-05-16 13:29:23 +0300

    Refactor

--- flutter_bloc_example/lib/presentations/screens/weather_screen.dart
+++ flutter_bloc_example/lib/presentations/screens/weather_screen.dart
@@ -1,10 +1,8 @@
-import 'dart:convert';
 import 'dart:ui';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:flutter_bloc_example/presentations/widgets/additional_info_item.dart';
 import 'package:flutter_bloc_example/presentations/widgets/hourly_forecast_item.dart';
-import 'package:http/http.dart' as http;
 import 'package:flutter_bloc_example/secrets.dart';
 
 class WeatherScreen extends StatefulWidget {
@@ -17,31 +15,9 @@ class WeatherScreen extends StatefulWidget {
 class _WeatherScreenState extends State<WeatherScreen> {
   late Future<Map<String, dynamic>> weather;
 
-  Future<Map<String, dynamic>> getCurrentWeather() async {
-    try {
-      String cityName = 'Damascus';
-      final res = await http.get(
-        Uri.parse(
-          'https://api.openweathermap.org/data/2.5/forecast?q=$cityName&APPID=$openWeatherAPIKey',
-        ),
-      );
-
-      final data = jsonDecode(res.body);
-
-      if (data['cod'] != '200') {
-        throw 'An unexpected error occurred';
-      }
-
-      return data;
-    } catch (e) {
-      throw e.toString();
-    }
-  }
-
   @override
   void initState() {
     super.initState();
-    weather = getCurrentWeather();
   }
 
   @override
@@ -54,14 +30,7 @@ class _WeatherScreenState extends State<WeatherScreen> {
         ),
         centerTitle: true,
         actions: [
-          IconButton(
-            onPressed: () {
-              setState(() {
-                weather = getCurrentWeather();
-              });
-            },
-            icon: const Icon(Icons.refresh),
-          ),
+          IconButton(onPressed: () {}, icon: const Icon(Icons.refresh)),
         ],
       ),
       body: FutureBuilder(
