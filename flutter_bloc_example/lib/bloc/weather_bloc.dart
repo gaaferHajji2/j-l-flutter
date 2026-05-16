eea5687f7b34fee93072ff91701bb1468ee25251
JafarLoka <gaafer.hajji1995@gmail.com>
2026-05-16 13:24:09 +0300

    Create the weaher handling

--- flutter_bloc_example/lib/bloc/weather_bloc.dart
+++ flutter_bloc_example/lib/bloc/weather_bloc.dart
@@ -1,13 +1,29 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter/foundation.dart';
+import 'package:flutter_bloc_example/data/repository/weather_repository.dart';
+import 'package:flutter_bloc_example/model/weather_model.dart';
 
 part 'weather_event.dart';
 part 'weather_state.dart';
 
 class WeatherBloc extends Bloc<WeatherEvent, WeatherState> {
-  WeatherBloc() : super(WeatherInitial()) {
-    on<WeatherEvent>((event, emit) {
-      // TODO: implement event handler
-    });
+  final WeatherRepository weatherRepository;
+  WeatherBloc({required this.weatherRepository}) : super(WeatherInitial()) {
+    on<WeatherFetched>(onWeatherFetched);
+  }
+
+  void onWeatherFetched(
+    WeatherFetched event,
+    Emitter<WeatherState> emit,
+  ) async {
+    emit(WeatherLoading());
+    try {
+      final weather = await this.weatherRepository.getCurrentWeather(
+        "Damascus",
+      );
+      emit(WeatherSuccess(weatherModel: weather));
+    } catch (e) {
+      emit(WeatherFailure(error: e.toString()));
+    }
   }
 }
