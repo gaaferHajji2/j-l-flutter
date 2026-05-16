8852b13874cf0502d16fd9d5e3e2668314a98990
JafarLoka <gaafer.hajji1995@gmail.com>
2026-05-16 12:36:29 +0300

    Create the weather state

--- flutter_bloc_example/lib/bloc/weather_state.dart
+++ flutter_bloc_example/lib/bloc/weather_state.dart
@@ -4,3 +4,9 @@ part of 'weather_bloc.dart';
 sealed class WeatherState {}
 
 final class WeatherInitial extends WeatherState {}
+
+final class WeatherSucess extends WeatherState {}
+
+final class WeatherFailure extends WeatherState {}
+
+final class WeatherLoading extends WeatherState {}
