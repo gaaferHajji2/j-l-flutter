5685ed5815200f4bfff8624d568fefbe876b49b5
JafarLoka <gaafer.hajji1995@gmail.com>
2026-06-13 16:42:52 +0300

    Build the animated alignment sample

--- flutter_animation_01/lib/pages/alignment_animation_sample.dart
+++ flutter_animation_01/lib/pages/alignment_animation_sample.dart
@@ -9,8 +9,69 @@ class AlignmentAnimationSample extends StatefulWidget {
 }
 
 class _AlignmentAnimationSampleState extends State<AlignmentAnimationSample> {
+  int _jerryAligned = 0;
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      appBar: AppBar(title: const Text("Animated Alignment Sample")),
+      body: Stack(
+        children: <Widget>[
+          AnimatedAlign(
+            alignment: getNextAlignment(_jerryAligned),
+            duration: const Duration(milliseconds: 500),
+            child: Container(
+              width: 65,
+              height: 100,
+              color: Colors.transparent,
+              child: Image.asset("assets/tom.png"),
+            ),
+          ),
+
+          AnimatedAlign(
+            alignment: getNextAlignment(_jerryAligned + 1),
+            duration: const Duration(milliseconds: 500),
+            child: Container(
+              width: 65,
+              height: 100,
+              color: Colors.transparent,
+              child: Image.asset("assets/jerry.png"),
+            ),
+          ),
+        ],
+      ),
+
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          setState(() {
+            _jerryAligned += 1;
+          });
+        },
+        child: Icon(Icons.shuffle),
+      ),
+    );
+  }
+
+  Alignment getNextAlignment(int alignemt) {
+    switch (alignemt) {
+      case 1:
+        return Alignment.topCenter;
+      case 2:
+        return Alignment.topRight;
+      case 3:
+        return Alignment.centerLeft;
+      case 4:
+        return Alignment.center;
+      case 5:
+        return Alignment.centerRight;
+      case 6:
+        return Alignment.bottomLeft;
+      case 7:
+        return Alignment.bottomCenter;
+      case 8:
+        return Alignment.bottomRight;
+      default:
+        _jerryAligned = 0;
+        return Alignment.topLeft;
+    }
   }
 }
