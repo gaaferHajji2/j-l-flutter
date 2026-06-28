4447344a3c6cfe97582e138ede8ba4f9d72f6476
JafarLoka <gaafer.hajji1995@gmail.com>
2026-06-28 11:45:16 +0300

    Remove unused imports

--- flutter_animation_01/lib/pages/container_animation_sample.dart
+++ flutter_animation_01/lib/pages/container_animation_sample.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
 
 class ContainerAnimationSample extends StatefulWidget {
   const ContainerAnimationSample({super.key});
