4a2c525df624b38c84007b826286c8d17276a344
JafarLoka <gaafer.hajji1995@gmail.com>
2026-06-28 12:00:41 +0300

    Build the sample of animated text style

--- flutter_animation_01/lib/pages/animated_text_sample.dart
+++ flutter_animation_01/lib/pages/animated_text_sample.dart
@@ -9,6 +9,8 @@ class AnimatedTextStyleSample extends StatefulWidget {
 }
 
 class _AnimatedTextStyleSampleState extends State<AnimatedTextStyleSample> {
+  double fontSize = 10.0;
+  Color? textColor = Colors.grey[400];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,7 +18,38 @@ class _AnimatedTextStyleSampleState extends State<AnimatedTextStyleSample> {
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         crossAxisAlignment: CrossAxisAlignment.center,
-        children: <Widget>[Text("Jafar Loka Sample Data")],
+        children: <Widget>[
+          AnimatedDefaultTextStyle(
+            style: TextStyle(fontSize: fontSize, color: textColor),
+            duration: Duration(milliseconds: 500),
+            child: Text("Jafar Loka Text"),
+          ),
+
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              IconButton(
+                onPressed: () {
+                  setState(() {
+                    fontSize = 24.0;
+                    textColor = Colors.orange[400];
+                  });
+                },
+                icon: Icon(Icons.add),
+              ),
+
+              IconButton(
+                onPressed: () {
+                  setState(() {
+                    fontSize = 10.0;
+                    textColor = Colors.grey[400];
+                  });
+                },
+                icon: Icon(Icons.remove),
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
